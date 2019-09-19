c1d0c7f8dac8fd6641af5740967ef974d5d24c6f
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-19 18:58:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ end
 end 
 
 def execute_transaction 
-if self.valid? && self.sender.balance >= self.amount 
+if self.valid? && self.sender.balance >= self.amount && self.status == 'pending'
 self.sender.balance -= self.amount
 self.receiver.balance += self.amount 
 self.status = 'complete'
