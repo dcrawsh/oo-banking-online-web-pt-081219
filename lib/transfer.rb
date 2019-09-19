8491c2e70b343d551bf6322c65062a7f46cb1202
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-19 18:47:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ end
 end 
 
 def execute_transaction 
-if self.valid? 
+if self.valid? && self.sender.balance >= self.amount 
 self.sender.balance -= self.amount
 self.receiver.balance += self.amount 
 self.status = 'complete'
@@ -24,7 +24,6 @@ self.amount = 0
 else 
 self.status = 'rejected'
 "Transaction rejected. Please check your account balance."
-
 end 
 end 
 
