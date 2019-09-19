7668b2bc64f6812de447cb9d2f967bd031f90482
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-19 18:32:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,10 @@
 class Transfer
-attr_accessor :sender, :receiver, :status 
+attr_accessor :sender, :receiver, :status, :amount 
 def initialize(person_1, person_2, amount)
 @sender = person_1  
 @receiver = person_2
 @status = 'pending'
+@amount = amount 
 end
 
 end 
